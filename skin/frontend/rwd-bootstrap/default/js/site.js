63d38f0423b0f76c05364fd555c73a70e6109c43
Alexandre Parra da Silva <alexpds@gmail.com>
2016-09-26 18:13:18 +0100

    Several Improvments: - Qty plus minus - CSS Clean Up

--- skin/frontend/rwd-bootstrap/default/js/site.js
+++ skin/frontend/rwd-bootstrap/default/js/site.js
@@ -1,1 +1,32 @@
-var $j=jQuery.noConflict();$j(document).ready(function(n){});
+jQuery.noConflict();
+
+jQuery(document).ready(function($){
+
+	// Product View Qty +/-
+	var $qty = $('#qty');
+	if( $qty.length ) {
+		var $addToCart = $qty.closest('.add-to-cart');
+
+		$addToCart.addClass('plus-minus');
+
+		var qtyMinus = $('<span class="qty-btn qty-minus">&#8211;</span>');
+		$qty.before(qtyMinus);
+
+		var qtyPlus = $('<span class="qty-btn qty-plus">&#43;</span>');
+		$qty.after(qtyPlus);
+
+		$addToCart.on('click', '.qty-btn', function(ev){
+			var btn = $(this);
+			var qty = parseInt($qty.val());
+			if( btn.hasClass('qty-minus') && qty > 1 ) {
+				$qty.val(qty-1);
+			}
+			if( btn.hasClass('qty-plus') ) {
+				$qty.val(qty+1);
+			}
+		});
+
+	}
+
+
+});
